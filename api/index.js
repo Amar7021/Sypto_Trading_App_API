8d839163ea1af6a3be368f4357fc0b3cb3fe3e38
Amar7021 <amarhere1122@gmail.com>
2024-04-13 18:20:00 +0530

    Second commit

--- api/index.js
+++ api/index.js
@@ -1,22 +1,11 @@
 import app from "./app.js"
-import connectToDB from "./db/connect.js"
 import dotenv from "dotenv"
 
 dotenv.config({ path: "./.env" })
 
 const PORT = process.env.PORT || 5000
 
-// Connect to DB
-connectToDB()
-  .then(() => {
-    app.on("error", (error) => {
-      console.log("Error occurred while connecting to DB: ", error)
-    })
-
-    app.listen(PORT, () => {
-      console.log(`Server is running on http://localhost:${PORT}`)
-    })
-  })
-  .catch((error) => {
-    console.log("MongoDB connection failed!!!", error)
-  })
+// Listen to server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`)
+})
